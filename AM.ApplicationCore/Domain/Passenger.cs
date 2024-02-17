bbd40bae8c854bea07b3d93c1c1e60a35f7f01ce
azizabdouli <azizabdouli601@gmail.com>
2024-02-18 00:39:37 +0100

    Entité

---
+++ AM.ApplicationCore/Domain/Passenger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.ApplicationCore.Domain
+{
+    
+    public class Passenger
+    {
+        public string PassportNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string EmailAdress { get; set; }
+        public string PhoneNumber { get; set; }
+        public virtual void PassengerType()
+        {
+            Console.WriteLine("I am a passenger");
+        }
+        public override string ToString()
+        {
+            return $"Passport Number: {PassportNumber}, First Name: {FirstName}, " +
+                   $"Last Name: {LastName}, Birth Date: {BirthDate}, " +
+                   $"Email Address: {EmailAdress}, Phone Number: {PhoneNumber}";
+        }
+        public Boolean CheckProfile(string prenom, string nom, string email = null)
+        {
+            if (email != null)
+                return (prenom == FirstName && nom == LastName && email == EmailAdress);
+            else
+                return (prenom == FirstName && nom == LastName);
+
+        }
+
+
+    }
+}
---
+++ AM.UI.Console/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using AM.ApplicationCore.Domain;
+
+Plane p = new Plane();
+p.Capacity = 100;
+Console.WriteLine(p.Capacity);
+p.ManufactureDate = DateTime.Now;
+p.PlaneType = PlaneType.Boeing;
+Passenger p1 = new Passenger() {FirstName="Aziz",LastName="Abdouli",EmailAdress="aziz.abdouli@esprit.tn"};
+Traveller t1 = new Traveller();
+Staff s1 = new Staff();
+p1.PassengerType();
+t1.PassengerType();
+s1.PassengerType();
+
+
+
+
+
+
